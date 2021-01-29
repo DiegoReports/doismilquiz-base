102fabc1aff38209a13739a553e599cd347228d9
unknown <diego.reports@gmail.com>
2021-01-29 11:55:37 -0300

    Second Day - Linter, Route and State

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 /* const BackgroundImage = styled.div`
   background-image: url(${db.bg});
@@ -24,17 +28,56 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
-    <QuizBackground backgroundImage = {db.bg}>
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>DoisMilQuiz - Modelo Base</title>
+
+        <meta name="title" content="Curiosidades Ano 2000" />
+        <meta name="description" content="Teste seus conhecimentos nesse AppQuiz! Perguntas sobre assuntos nostálgicos." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://doismilquiz-base.diegoreports.vercel.app/" />
+        <meta property="og:title" content="Curiosidades Ano 2000" />
+        <meta property="og:description" content="Teste seus conhecimentos nesse AppQuiz! Perguntas sobre assuntos nostálgicos." />
+        <meta property="og:image" content="" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://doismilquiz-base.diegoreports.vercel.app/" />
+        <meta property="twitter:title" content="Curiosidades Ano 2000" />
+        <meta property="twitter:description" content="Teste seus conhecimentos nesse AppQuiz! Perguntas sobre assuntos nostálgicos." />
+        <meta property="twitter:image" content="" />
+      </Head>
+
       <QuizContainer>
         <Widget>
           <Widget.Header>
-            <h1>Curiosidade Ano 2000</h1>
+            <h1>Curiosidades Ano 2000</h1>
           </Widget.Header>
           <Widget.Content>
-
-            <p>Descrição aleatorio sobre oque se trata esse quiz.</p>
-
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do React');
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  console.log(infosDoEvento.target.value);
+                  // State
+                  // name = infosDoEvento.target.value
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Diz ai seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
@@ -50,7 +93,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      < GitHubCorner projectUrl="https://github.com/DiegoReports"/>
+      <GitHubCorner projectUrl="https://github.com/DiegoReports" />
     </QuizBackground>
   );
 }
